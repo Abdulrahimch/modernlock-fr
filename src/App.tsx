e6799a852555c5e3c7acaccf4218d41afe4c65c8
Shiha <abdulrahimcisco92@gmail.com>
2023-11-21 19:03:17 +0300

    adding context

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,8 @@ import { Box } from '@material-ui/core';
 import { SnackBarProvider } from './context/useSnackbarContext';
 import { AuthProvider } from './context/useAuthContext';
 import Test from './pages/Test';
-import Agent from './pages/Agent/Agent';
-import ClientDetails from './pages/Agent/ClientDetails/ClientDetails';
-import Home from './pages/Home/Home';
-import Owner from './pages/Owner/Owner';
-import Login from './pages/Login/Login';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import Header from './components/Header/Header';
-import NewDevices from './pages/Agent/NewDevices/NewDevices';
+
+// import Header from './components/Header/Header';
 
 function App (): JSX.Element {
     return (
@@ -19,16 +13,10 @@ function App (): JSX.Element {
             <BrowserRouter>
                 <SnackBarProvider>
                     <AuthProvider>
-                        <Header />
+                        {/* <Header /> */}
                             <Switch>
                                 <Box style={{ overflow: 'hidden' }}>
                                     <Route exact path="/" component={Test} />
-                                    <ProtectedRoute exact path="/home/owner" component={Owner} role="owner" />
-                                    <ProtectedRoute exact path="/home/agent" component={Agent} role="agent" />
-                                    <Route exact path="/home" component={Home} />
-                                    <Route exact path="/login" component={Login} />
-                                    <Route exact path="/client/details/:id" component={ClientDetails} />
-                                    <Route exact path="/home/agent/new-devices" component={NewDevices} />
                                 </Box>
                             </Switch>
                     </AuthProvider>
---
+++ src/context/useAuthContext.tsx
@@ -0,0 +1,60 @@
+import { useState, useContext, createContext, FunctionComponent, useEffect, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { AuthApiData, AuthApiDataSuccess } from '../interface/AuthApiData';
+import { User } from '../interface/User';
+import loginWithCookies from '../helpers/APICalls/loginWithCookies';
+import logoutAPI from '../helpers/APICalls/logout';
+
+interface IAuthContext {
+  loggedInUser: User | null | undefined;
+  updateLoginContext: (data: AuthApiDataSuccess) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<IAuthContext>({
+  loggedInUser: undefined,
+  updateLoginContext: () => null,
+  logout: () => null,
+});
+
+export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
+  const [loggedInUser, setLoggedInUser] = useState<User | null | undefined>();
+  const history = useHistory();
+
+  const updateLoginContext = useCallback(
+    (data: AuthApiDataSuccess) => {
+      setLoggedInUser(data.user);
+      data.user.role === "owner" ? history.push("/home/owner") : history.push("/home/agent")
+    },
+    [history],
+  );
+
+  const logout = useCallback(async () => {
+    await logoutAPI()
+      .then(() => {
+        history.push('/login');
+        setLoggedInUser(null);
+      })
+      .catch((error: any) => console.error(error));
+  }, [history]);
+
+  useEffect(() => {
+    const checkLoginWithCookies = async () => {
+      await loginWithCookies().then((data: AuthApiData) => {
+        if (data.success) {
+          // data.success.user.role === "owner" ? history.push("/home/owner") : data.success.user.role === "agent" ? history.push("/home/agent") : history.push("/home")
+          updateLoginContext(data.success);
+        } else {
+          setLoggedInUser(null);
+        }
+      });
+    };
+    checkLoginWithCookies();
+  }, [updateLoginContext, history]);
+
+  return <AuthContext.Provider value={{ loggedInUser, updateLoginContext, logout }}>{children}</AuthContext.Provider>;
+};
+
+export function useAuth(): IAuthContext {
+  return useContext(AuthContext);
+}
