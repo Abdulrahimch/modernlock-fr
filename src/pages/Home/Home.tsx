b33f6e12b053e57698d068dbf67cb93b65a62193
Shiha <abdulrahimcisco92@gmail.com>
2023-11-28 19:52:17 +0300

    craeting swiper and making it responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,36 @@ import { ButtonInterface } from "../../interface/SubHeader";
 import { useHistory } from "react-router";
 import useStyles from "./useStyles";
 import CustomButton from "../../components/CustomButton/CustomButton";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './styles.css';
+
 
 const Home = () => {
+
+    const breakpoints = {
+        // When window width is >= 768px
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        // When window width is >= 992px
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        // When window width is >= 1200px
+        1200: {
+          slidesPerView: 2.5,
+          spaceBetween: 30,
+        },
+      };
+
     const history = useHistory();
     const { root, pageTitle } = useStyles();
 
@@ -47,35 +75,49 @@ const Home = () => {
                                 <CustomButton btnText={btnText} style="btnTabStyle" onClick={onBtnClick}/>
                             ))}
                         </Grid>
-                        <Grid item>
-                            <Grid container direction="row" justifyContent="center" spacing={3}>
-                                {cards ? cards.map(({status, name, from, to, roomNo}) => (
-                                    <>
-                                        <Grid item xs={12} sm={12} md={4} lg={4}>
-                                            <Card style={{ borderRadius: "25px" }}>
-                                                <CardContent>
-                                                    <Typography gutterBottom style={{ fontWeight: 800, fontSize: 16, color: "red", marginBottom: "2em" }}>
-                                                        {status}
-                                                    </Typography>
-                                                    <Typography gutterBottom style={{ fontWeight: 800, fontSize: 16, marginBottom: "1em", textTransform: "capitalize" }}>
-                                                        {name} <br /> {`${from} -  ${to}`}
-                                                    </Typography>
-                                                    {/* <Typography style={{ fontWeight: 800, fontSize: 16 }}>
-                                                        {`${from} -  ${to}`} 
-                                                    </Typography> */}
-                                                    <Typography variant="body2" style={{ color: "secondary" }}>
-                                                        {`Room/Apart No: ${roomNo}`}
-                                                    </Typography>
-                                                </CardContent>
-                                                <CardActions>
-                                                    <Button size="small">Share</Button>
-                                                    <Button size="small">Learn More</Button>
-                                                </CardActions>
-                                            </Card>
-                                        </Grid>
-                                    </>
-                                )): "None"}
+                        <Grid item style={{ width: "50%" }}>
+                            <Grid container direction="row" justifyContent="center" style={{ width: "100%" }}>
+                                <Swiper
+                                    breakpoints={breakpoints}
+                                    pagination={{
+                                    clickable: true,
+                                    }}
+                                    modules={[Pagination]}
+                                    className={"mySwiper"}
+                                >
+                                    {cards ? cards.map(({status, name, from, to, roomNo}) => (
+                                        <>
+                                            <SwiperSlide>
+                                                <Grid item xs={12} sm={12}  style={{ width: "100%" }}>
+                                                    <Card style={{ borderRadius: "25px" }}>
+                                                        <CardContent>
+                                                            <Typography gutterBottom style={{ fontWeight: 800, fontSize: 16, color: "red", marginBottom: "2em" }}>
+                                                                {status}
+                                                            </Typography>
+                                                            <Typography gutterBottom style={{ fontWeight: 800, fontSize: 16, marginBottom: "1em", textTransform: "capitalize" }}>
+                                                                {name} <br /> {`${from} -  ${to}`}
+                                                            </Typography>
+                                                            {/* <Typography style={{ fontWeight: 800, fontSize: 16 }}>
+                                                                {`${from} -  ${to}`} 
+                                                            </Typography> */}
+                                                            <Typography variant="body2" style={{ color: "secondary" }}>
+                                                                {`Room/Apart No: ${roomNo}`}
+                                                            </Typography>
+                                                        </CardContent>
+                                                        <CardActions>
+                                                            <Button size="small">Share</Button>
+                                                            <Button size="small">Learn More</Button>
+                                                        </CardActions>
+                                                    </Card>
+                                                </Grid>
+                                            </SwiperSlide>
+                                        </>
+                                    )): "None"}
+                                </Swiper> 
                             </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Typography>ASK US</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
