61f990b8c05f31fdfe793a6e0f8190d66378e2e2
Shiha <abdulrahimcisco92@gmail.com>
2023-12-04 21:20:40 +0300

    adding interfaces for reservation and property

--- src/interface/Property.ts
+++ src/interface/Property.ts
@@ -1,4 +1,5 @@
-interface Property {
+export interface Property {
+    _id: string;
     name: string;
     address?: string;
     phone?: string
--- src/interface/Reservation.ts
+++ src/interface/Reservation.ts
@@ -3,7 +3,18 @@ export interface Reservation {
     email: string;
     name?: string;
     propertyId?: string;
-    checkin?: string;
-    checkout?: string;
+    checkin: string;
+    checkout: string;
     reservationId?: string;
+};
+
+interface ReservationApiDataSuccess {
+    reservations?: Reservation[];
+    reservation?: Reservation
 }
+
+export interface ReservationApiData {
+    error?: { message: string };
+    success?: ReservationApiDataSuccess;
+};
+  
