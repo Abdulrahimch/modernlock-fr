1f8e7cbcd0fb964b2f631052f7cdf038d5244476
Shiha <abdulrahimcisco92@gmail.com>
2023-12-04 21:18:47 +0300

    getReservations api

--- src/helpers/APICalls/reservation.ts
+++ src/helpers/APICalls/reservation.ts
@@ -1,11 +1,20 @@
 import axios from './axiosConfig';
 import { PropertyApiData } from '../../interface/Property';
+import { ReservationApiData } from '../../interface/Reservation';
 
-export const postReservation = async (propertyId: string, inputs: any): Promise<PropertyApiData> => {
-    
-    return await axios.post(`/api/v1/reservations${propertyId}`, inputs)
+export const postReservation = async (propertyId: string, inputs: any): Promise<ReservationApiData> => {
+    return await axios.post(`/api/v1/reservations/${propertyId}`, inputs)
         .then((res) => res.data)
         .catch(() => ({
             error: { message: 'Unable to connect to server. Please try again' }
         }))
 };
+
+export const getReservations = async (propertyId: string): Promise<ReservationApiData> => {
+    return await axios.get(`/api/v1/reservations/${propertyId}`)
+        .then((res) => res.data)
+        .catch(() => ({
+            error: { message: 'Unable to connect to server. Please try again' }
+        }))
+};
+
