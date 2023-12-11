27030a0faf58c503fc0fcd636afe12aa3489edea
Shiha <abdulrahimcisco92@gmail.com>
2023-12-11 23:18:14 +0300

    adding getProperties to Authcontext

--- src/context/useAuthContext.tsx
+++ src/context/useAuthContext.tsx
@@ -4,27 +4,41 @@ import { AuthApiData, AuthApiDataSuccess } from '../interface/AuthApiData';
 import { User } from '../interface/User';
 import loginWithCookies from '../helpers/APICalls/loginWithCookies';
 import logoutAPI from '../helpers/APICalls/logout';
+import { Property, PropertyApiDataSuccess } from "../interface/Property";
+import { getProperties } from '../helpers/APICalls/properties';
 
 interface IAuthContext {
   loggedInUser: User | null | undefined;
+  userProperties: Property [] | null | undefined;
   updateLoginContext: (data: AuthApiDataSuccess) => void;
+  updateUserProperties: (data: PropertyApiDataSuccess) => void;
   logout: () => void;
 }
 
 export const AuthContext = createContext<IAuthContext>({
   loggedInUser: undefined,
+  userProperties: [],
   updateLoginContext: () => null,
+  updateUserProperties: () => null,
   logout: () => null,
 });
 
 export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   const [loggedInUser, setLoggedInUser] = useState<User | null | undefined>();
+  const [userProperties, setUserProperties] = useState<Property [] | null | undefined >([])
   const history = useHistory();
 
   const updateLoginContext = useCallback(
     (data: AuthApiDataSuccess) => {
       setLoggedInUser(data.user);
       data.user.accountType === "owner" ? history.push("/home/owner") : history.push("/home/client")
+    },
+    [history],
+  );
+
+  const updateUserProperties = useCallback(
+    (data: PropertyApiDataSuccess) => {
+      setUserProperties(data.properties);
     },
     [history],
   );
@@ -42,7 +56,6 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
     const checkLoginWithCookies = async () => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
-          // data.success.user.role === "owner" ? history.push("/home/owner") : data.success.user.role === "agent" ? history.push("/home/agent") : history.push("/home")
           updateLoginContext(data.success);
         } else {
           setLoggedInUser(null);
@@ -52,7 +65,19 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
     checkLoginWithCookies();
   }, [updateLoginContext, history]);
 
-  return <AuthContext.Provider value={{ loggedInUser, updateLoginContext, logout }}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    getProperties().then((data) => {
+        if (data.error) {
+            console.log(data.error.message);
+        } else if (data.success){
+            updateUserProperties(data.success)
+        } else {
+            console.log('An unexpected error occurred. Please try again !');
+        }
+    });
+}, [updateUserProperties])
+
+  return <AuthContext.Provider value={{ loggedInUser, updateLoginContext, logout, updateUserProperties, userProperties }}>{children}</AuthContext.Provider>;
 };
 
 export function useAuth(): IAuthContext {
